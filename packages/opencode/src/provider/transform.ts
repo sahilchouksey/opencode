69bff2ac0a68f4657f676431c426ee1967214bef
Sahil <sahilch696969@gmail.com>
2025-08-08 20:33:57 +0530

    feat: added --auth-filter param

--- packages/opencode/src/provider/transform.ts
+++ packages/opencode/src/provider/transform.ts
@@ -106,3 +106,8 @@ export namespace ProviderTransform {
     // }
   }
 }
+
+ export function topP(_providerID: string, modelID: string) {
+    if (modelID.toLowerCase().includes("qwen")) return 1
+    return undefined
+}
